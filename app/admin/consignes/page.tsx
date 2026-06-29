6f65e66433883916b71341099a163891419f0534
flaviendu7 <67783316+flaviendu7@users.noreply.github.com>
2026-06-29 22:34:25 +0200

    Delete app directory

--- app/admin/consignes/page.tsx
+++
@@ -1,2 +0,0 @@
-import AppShell from '@/components/AppShell'
-export default function AdminConsignes(){return <AppShell><h1 className="text-3xl font-extrabold mb-6">Administration consignes</h1><div className="card p-6"><button className="btn">Nouvelle consigne</button></div></AppShell>}
